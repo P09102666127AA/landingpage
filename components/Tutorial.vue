044699b966d0306a21fc5e0de035bd87327b6982
pooria <pa2002.tir@gmail.com>
2023-08-12 11:14:33 +0330

    creat landing page for desktop anf mobile

---
+++ components/Tutorial.vue
@@ -0,0 +1,456 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <div class="card">
+      <div class="card-image">
+        <figure class="image is-3by1">
+          <img src="headimage.jpg" alt="Placeholder image" />
+        </figure>
+      </div>
+      <div class="is-overlay">
+        <div class="columns is-mobile is-centered is-vcentered">
+          <div class="column is-6-desktop">
+            <div class="bottom-circle-mobile has-text-centered">
+  <div class="upper-Square has-text-centered">
+    <br>
+    <div class="center is-size-5 is-size-7-mobile">
+      <strong style="color: rgb(0, 0, 0)">فروشگاه اینترنتی</strong>
+      <strong style="color: blue">فناورداده</strong>
+    </div>
+  </div>
+</div>
+            <p
+              class="is-size-3 is-size-7-mobile has-text-white has-text-weight-bold has-text-centered text-titr"
+            >
+              سرعت چند برابری رو با فروشگاه ما تجربه کنید!
+            </p>
+            <p
+              class="is-size-5 is-size-7-mobile has-text-centered has-text-white text-header-img-mobile text-head-desktop"
+            >
+              از شروع راه اندازی سایت و فروشگاه اینترنتی تا رسیدن به فروش بالا و
+              سیستم سازی در کنارتان هستیم
+            </p>
+            <div class="has-text-centered">
+              <button class="button is-primary is-size-7-mobile">
+                نمایش دمو
+              </button>
+            </div>
+          </div>
+          <div class="column has-text-left is-4-desktop is-4-mobile pl-5">
+            <img src="mypc.png" class="border" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="columns is-mobile is-centered is-vcentered my-6">
+      <div class="column has-text-centered">
+        <img src="speed-important.png" alt="" />
+      </div>
+      <div class="column pl-4">
+        <h1
+          class="is-size-4 is-size-6-mobile has-text-primary has-text-weight-bold"
+        >
+          چرا بالا بودن سرعت وب سایت اهمیت دارد؟
+        </h1>
+        <ul class="mt-5 is-size-5 is-size-7-mobile">
+          <li>موثر ترین مولفه در بهبود رتبه ی گوگل</li>
+        </ul>
+        <ul class="mt-5 is-size-5 is-size-7-mobile">
+          <li>کاهش نرخ برگشتی یا bounce rate</li>
+        </ul>
+        <ul class="mt-5 is-size-5 is-size-7-mobile">
+          <li>بالا بردن نرخ تبدیل</li>
+        </ul>
+        <ul class="mt-5 is-size-5 is-size-7-mobile">
+          <li>رضایت بازدید کننده ها</li>
+        </ul>
+        <ul class="mt-5 is-size-5 is-size-7-mobile">
+          <li>و در نتیجه افزایش بازدید و افزایش فروش</li>
+        </ul>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-image">
+        <figure class="image is-3by1">
+          <img src="offer.jpg" alt="Placeholder image" />
+        </figure>
+      </div>
+      <div class="card-content is-overlay">
+        <div class="columns is-mobile is-centered">
+          <div class="column">
+            <p
+              class="is-size-3 is-size-6-mobile has-text-white has-text-weight-bold has-text-centered"
+            >
+              فقط تا جمعه 20 مرداد وقت دارید از فروشگاه ما با تخفیف استفاده
+              کنید!
+            </p>
+            <p
+              class="is-size-5 is-size-7-mobile has-text-centered has-text-weight-bold has-text-white pt-6 is-desktop-mobile is-hidden-mobile"
+            >
+              شماره موبایل خود را وارد نمایید!
+            </p>
+            <div
+              class="field is-grouped has-text-centered is-desktop-mobile is-hidden-mobile pt-6"
+            >
+              <p class="control is-expanded">
+                <input
+                  class="input size-input-desktop"
+                  type="text"
+                  placeholder="شماره موبایل"
+                />
+
+                <a class="button is-link size-buttion"> تایید! </a>
+              </p>
+            </div>
+          </div>
+          <div class="column has-text-left is-2-desktop offer-image">
+            <img src="stars-offer.png" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      class="box mt-6 has-text-centered box-sing-up is-Visible-mobile is-hidden-desktop"
+    >
+      <p
+        class="is-size-5 is-size-7-mobile has-text-centered has-text-weight-bold mt-4 mb-6"
+      >
+        شماره موبایل خود را وارد نمایید!
+      </p>
+      <div class="field is-grouped has-text-centered">
+        <p class="control is-expanded">
+          <input
+            class="input size-input-mobile"
+            type="text"
+            placeholder="شماره موبایل"
+          />
+
+          <a class="button is-link size-buttion-mobile"> تایید! </a>
+        </p>
+      </div>
+    </div>
+    <div
+      class="box text-box-mobile text-box-desktop is-size-5 is-size-6-mobile mt-6"
+    >
+      <p class="has-text-weight-bold">ویژگی ها و امکانات سایت:</p>
+    </div>
+
+    <div class="columns is-mobile is-centered is-vcentered color-box">
+      <div class="column has-text-centered has-text-left is-hidden-mobile">
+        <img
+          src="circle-left-blue.png"
+          alt="circle-left-blue"
+          class="circle-half"
+        />
+      </div>
+      <div
+        class="column is-four-fifths-mobile is-8-desktop has-text-centered box has-background-link-dark"
+      >
+        <div class="columns is-mobile is-centered is-vcentered width-box">
+          <div class="column has-text-white">
+            <p class="has-text-weight-bold">PWA</p>
+            <p>
+              بدون ساخت برنامه ایی برای موبایل می توانید سایتتون رو مثل یک
+              برنامه تنظیم کنید که مانند برنامه برای یک برنامه در گوشی نصب شود
+              برنامه برای کاربر رفتار کند
+            </p>
+          </div>
+          <div class="column">
+            <img src="mobile.png" class="border" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="columns is-mobile is-centered is-vcentered color-box">
+      <div
+        class="column is-four-fifths-mobile is-8-desktop has-text-centered box has-background-grey-light"
+      >
+        <div class="columns is-mobile is-centered is-vcentered width-box">
+          <div class="column has-text-white">
+            <p class="has-text-weight-bold">خدمات پیشرفته</p>
+            <p>
+              خدماتی که شامل ساخت برنامه پیش رونده (PWA) و قراردادن برنامه در
+              بازارچه اندرویدی می باشد
+            </p>
+          </div>
+          <div class="column">
+            <img src="desktop-web.jpg" class="border" />
+          </div>
+        </div>
+      </div>
+      <div class="column has-text-centered has-text-right is-hidden-mobile">
+        <img
+          src="circle-right-gray.png"
+          alt="circle-right-gray"
+          class="circle-half"
+        />
+      </div>
+    </div>
+
+    <div class="columns is-mobile is-centered is-vcentered color-box">
+      <div class="column has-text-centered has-text-left is-hidden-mobile">
+        <img
+          src="circle-left-blue.png"
+          alt="circle-left-blue"
+          class="circle-half"
+        />
+      </div>
+      <div
+        class="column is-four-fifths-mobile is-8-desktop has-text-centered box has-background-link-dark"
+      >
+        <div class="columns is-mobile is-centered is-vcentered width-box">
+          <div class="column has-text-white">
+            <p class="has-text-weight-bold">مکان یابی (GPS)</p>
+            <p>
+              قابلیت دریافت آدرس دقیق از طریق مکان یاب (GPS) از کاربران و ارسال
+              محصولات و دیگر نقاط مختلف مورد نیاز پنل یا سیستم
+            </p>
+          </div>
+          <div class="column">
+            <img src="map.jpg" class="border" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="columns is-mobile is-centered is-vcentered color-box">
+      <div
+        class="column is-four-fifths-mobile is-8-desktop has-text-centered box has-background-grey-light"
+      >
+        <div class="columns is-mobile is-centered is-vcentered width-box">
+          <div class="column has-text-white">
+            <p class="has-text-weight-bold">سرویس پیامک</p>
+            <p>
+              قابلیت ارسال پیامک به هر نقطه از پنل یا سیستم و حتی ارسال پیام
+              برای کاربران
+            </p>
+          </div>
+          <div class="column">
+            <img src="service sms.jpg" class="border" />
+          </div>
+        </div>
+      </div>
+      <div class="column has-text-centered has-text-right is-hidden-mobile">
+        <img
+          src="circle-right-gray.png"
+          alt="circle-right-gray"
+          class="circle-half"
+        />
+      </div>
+    </div>
+
+    <div class="columns is-mobile is-centered is-vcentered color-box">
+      <div class="column has-text-centered has-text-left is-hidden-mobile">
+        <img
+          src="circle-left-blue.png"
+          alt="circle-left-blue"
+          class="circle-half"
+        />
+      </div>
+      <div
+        class="column is-four-fifths-mobile is-8-desktop has-text-centered box has-background-link-dark"
+      >
+        <div class="columns is-mobile is-centered is-vcentered width-box">
+          <div class="column has-text-white">
+            <p class="has-text-weight-bold">حسابداری و انبارداری</p>
+            <p>
+              این نرم افزار بین حسابداری و وب سایت فروشگاهی ارتباط پایداری
+              برقرار می کند و با ایجاد بستری مناسب امکان افزایش فروش به صورت
+              آنلاین را نیز فراهم می سازد
+            </p>
+          </div>
+          <div class="column">
+            <img src="Accounting.jpg" class="border" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      class="my-6 has-text-centered is-size-5 is-size-6-mobile has-text-weight-bold has-text-link-dark"
+    >
+      برخی از ویژگی ها و امکانات دیگر سایت :
+    </div>
+    <div class="box box-Attributes container has-background-grey-lighter">
+      <p class="has-text-dark">
+        <img src="tik.png" width="50" height="50" />
+        <strong class="is-size-5 is-size-6-mobile has-text-black"
+          >امنیت:</strong
+        >
+
+        سایت شما بالاترین سطح پروتکل امنیتی SSI (بالاترین سطح امنیت که در آن
+        اطلاعات وارد شده کاربران در امنیت خواهد بود) را خواهد داشت.
+      </p>
+      <p class="has-text-dark mt-4 text-box-attributes">
+        <img src="tik.png" width="50" height="50" />
+        <strong class="is-size-5 is-size-6-mobile has-text-black">SPA:</strong>
+        یک برنامه متصل به سایت یا سایتی است که با بروزرسانی صفحه سایت داده های
+        جدید را از سرور با تاخیر بسیار کم و بدون تغییر ادرس صفحه را می گیرد و با
+        کاربر تعامل برقرار می کند.
+      </p>
+      <p class="has-text-dark mt-4">
+        <img src="tik.png" width="50" height="50" />
+        <strong class="is-size-5 is-size-6-mobile has-text-black"
+          >مدیریت مشتریان:</strong
+        >
+        مدیریت مشتریان شامل: حضور و اطلاعات مشتریان و امنیت اطلاعات مشتریان است.
+      </p>
+      <p class="has-text-dark mt-4">
+        <img src="tik.png" width="50" height="50" />
+        <strong class="is-size-5 is-size-6-mobile has-text-black"
+          >پرداخت:</strong
+        >
+        پرداخت شامل : پرداخت آنلاین ، تاریخچه پرداخت ها و قابلیت اتصال به چند
+        درگاه است.
+      </p>
+      <p class="has-text-dark mt-4 text-box-attributes">
+        <img src="tik.png" width="50" height="50" />
+        <strong class="is-size-5 is-size-6-mobile has-text-black"
+          >سرویس ایمیل:</strong
+        >
+        شامل خدماتی که هر چیزی که به ایمیل ختم شود شامل احراز هویت با ایمیل و
+        سرویس ایمیل شرکتی و اتصال نرم افزار به ایمیل سازمانی می باشد.
+      </p>
+      <div class="flash">
+        <img src="flash-icon.png" width="50" height="50" style="right: 30%" />
+      </div>
+    </div>
+
+    <h1
+      class="is-size-4 is-size-7-mobile has-text-weight-bold pt-6 has-text-centered"
+    >
+      نظر مشتریان در رابطه با سایت های ساخته شده توسط فناورداده:
+    </h1>
+
+    <div class="bottom-photo">
+      <img src="half-circle.png" alt="half-circle.png" />
+      <div class="columns is-mobile Comments ml-6">
+        <div class="column box has-text-right ml-4 box-email">
+          <p
+            class="has-text-weight-bold is-size-5 is-size-6-mobile has-text-link-dark"
+          >
+            نیلوفر عباسی :
+          </p>
+          <p class="is-size-6 is-size-7-mobile has-text-weight-bold pt-4">
+            سلام من کسب و کارم فروشگاهی هست و از فروشگاه ساز شما استفاده کردم
+            خیلی کامل و امکانات خوبی داره.ممنونم از زحماتتون
+          </p>
+        </div>
+        <div class="column box ml-4 box-email">
+          <p
+            class="has-text-weight-bold is-size-5 is-size-6-mobile has-text-link-dark"
+          >
+            علی :
+          </p>
+          <p class="is-size-6 is-size-7-mobile has-text-weight-bold pt-4">
+            سلام من از فروشگاه ساز شما استفاده می کنم و خیلی کامل و کاربری راحتی
+            داره ممنون از شما
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="columns is-mobile is-centered is-vcentered my-6">
+      <div class="column pr-4">
+        <img src="Customer satisfaction.jpg" alt="Customer satisfaction" />
+      </div>
+      <div class="column">
+        <h1 class="is-size-4 is-size-7-mobile has-text-weight-bold pl-6">
+          شرکت فناورداده همواره در تلاش است تا برای جلب رضایت مشتری تمام تلاش
+          خود را برای رساندن شما به فروش بالا و سیستم سازی فروشگاه کند.
+        </h1>
+      </div>
+    </div>
+
+    <div class="mt-6 half-circle-blue">
+      <img src="halfcircleblue.png" alt="halfcircle.png" />
+      <div
+        class="textup-logo has-text-centered is-size-4-desktop is-size-6-mobile has-text-weight-bold has-text-dark"
+      >
+        فروشگاه های ساخته شده توسط فناورداده :
+      </div>
+      <div class="circle-box has-background-link"></div>
+      <div class="circle-box-small has-background-link"></div>
+      <div class="box box-logo box-logo-mobile colmuns">
+        <div class="column logo">
+          <img src="logo.png (1).jpeg" alt="logo" />
+        </div>
+        <div class="column logo">
+          <img src="logo.png.jpeg" alt="logo" />
+        </div>
+        <div class="column logo">
+          <img src="zeytonshop.jpeg" alt="logo" />
+        </div>
+      </div>
+    </div>
+    <footer>
+      <div class="background-footer mt-6">
+        <div class="columns has-text-white">
+          <!-- Column 1 -->
+          <div class="column mt-6">
+            <h4
+              class="bd-footer-title has-text-weight-medium has-text-centered"
+            >
+              فروشگاه ما در فضای مجازی :
+            </h4>
+            <div class="mt-4">
+              <img
+                src="insta-logo2.png"
+                alt="insta-logo2"
+                class="apps-logo-one"
+              />
+              <img
+                src="telegram-logo.png"
+                alt="telegram-logo"
+                class="apps-logo-two"
+              />
+            </div>
+          </div>
+
+          <!-- Column 2 -->
+          <div class="column mt-6">
+            <h4
+              class="bd-footer-title has-text-weight-medium has-text-centered"
+            >
+              مجوز های شرکت فناورداده :
+            </h4>
+            <a href="#" class="mt-4" style="display: flex">
+              <img src="samandehi.png" alt="samandehi" class="namad-one" />
+              <img src="enamad.png" alt="enamad" class="namad-two" />
+            </a>
+          </div>
+
+          <!-- Column 3 -->
+          <div class="column mt-6">
+            <h4 class="has-text-centered has-text-weight-bold">
+              ارتباط با ما :
+            </h4>
+
+            <div
+              class="box has-text-centered telephone-box has-text-white mt-4 py-0"
+            >
+              <a href="#" class="pt-6 has-text-white">
+                <img src="telephone.png" class="telephonepng" />
+                021-31304027
+              </a>
+            </div>
+            <div
+              class="box has-text-centered telephone-box has-text-white mt-4 py-0"
+            >
+              <a href="#" class="has-text-white">
+                <img src="whatsapp.png" class="telephonepng" />
+                09021459616
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="box content has-text-centered has-text-white footer-box">
+          <p class="footer-text">کلیه حقوق این سایت محفوض می باشد.</p>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
